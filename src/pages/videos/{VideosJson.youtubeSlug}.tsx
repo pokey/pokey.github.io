8f1dc37fe029805385c0354c546c58ade56fb4bd
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-02-25 12:07:34 +0000

    More tweaks

--- src/pages/videos/{VideosJson.youtubeSlug}.tsx
+++ src/pages/videos/{VideosJson.youtubeSlug}.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import { graphql, PageProps } from "gatsby";
+import Helmet from "react-helmet";
+import { data } from "autoprefixer";
 
 interface DataProps {
-  videosJson: { youtubeSlug: string };
+  videosJson: { youtubeSlug: string; title: string };
 }
 
 export default function Component({
   data: { videosJson },
 }: PageProps<DataProps>) {
-  return <main className="text-2xl">{videosJson.youtubeSlug}</main>;
+  const { youtubeSlug, title } = videosJson;
+  return (
+    <main className="p-4 pt-3 max-w-prose sm:mx-auto bg-slate-200 rounded-lg">
+      <Helmet bodyAttributes={{ class: "bg-slate-100 p-2 sm:p-6 md:py-8" }} />
+      <title>{title}</title>
+      <h1 className="text-2xl text-center mt-0 mb-2">{title}</h1>
+      {youtubeSlug}
+    </main>
+  );
 }
 
 // This is the page query that connects the data to the actual component. Here you can query for any and all fields
